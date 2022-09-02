afe268ebcbb3c557440e085eb5a1be62309ba973
Parham Baghizadeh <parham.baghizadeh@yahoo.com>
2022-09-02 18:20:14 +0430

    Search Function for checkups refined

--- app/Http/Controllers/CheckupsController.php
+++ app/Http/Controllers/CheckupsController.php
@@ -12,8 +12,9 @@ use function PHPUnit\Framework\returnSelf;
 
 class CheckupsController extends Controller
 {
-    public function CreateCheckUp(Request $request){
-        try{
+    public function CreateCheckUp(Request $request)
+    {
+        try {
             $data = $request->validate([
                 'patient_id' => 'required|string',
                 'sympthoms' => 'required|string',
@@ -29,15 +30,15 @@ class CheckupsController extends Controller
             return response([
                 'message' => 'check up session created successfully.'
             ], 201);
-        }
-        catch(\Throwable $error){
+        } catch (\Throwable $error) {
             return response([
                 'message' => 'failed to create a new checkup session'
             ], 503);
         }
     }
 
-    public function GetAllCheckups(Request $request){
+    public function GetAllCheckups(Request $request)
+    {
         $checkups = Checkup::all();
         $res = [];
         foreach ($checkups as $c) {
@@ -47,26 +48,29 @@ class CheckupsController extends Controller
                 'patient_age' => $c->patient->age,
                 'owner_name' => $c->patient->owner->firstName . ' ' . $c->patient->owner->lastName,
                 'year' => $c->year,
-                'month' => $c->month ,
+                'month' => $c->month,
                 'day' => $c->day
             ];
         }
         return $res;
     }
 
-    public function FindCheckupsByPatientName(Request $request){
+    public function FindCheckupsByPatientName(Request $request)
+    {
         $patientName = $request->validate(['name' => 'string|required'])['name'];
-        $patients = Patient::where('name', 'like', '%'.$patientName.'%')->get();
+        $patients = Patient::where('name', 'like', '%' . $patientName . '%')->get();
         $checkups = [];
-        foreach($patients as $p){
+        foreach ($patients as $p) {
             $t_c = $p->checkups()->orderBy('year', 'desc')->orderBy('month', 'desc')->orderBy('day', 'desc')->get();
-            foreach($t_c as $c){
+            foreach ($t_c as $c) {
                 $checkups[] = [
                     'id' => $c->id,
                     'name' => $p->name,
                     'owner' => $p->owner->firstName . ' ' . $p->owner->lastName,
                     'age' => $p->age,
                     'gender' => $p->gender == 0 ? 'نر' : 'ماده',
+                    'type' => $p->type()->get(['name'])[0]->name,
+                    'race' => $p->race()->get(['name'])[0]->name,
                     'date' => $c->year . '/' . $c->month . '/' . $c->day,
                     'sympthoms' => $c->sympthoms,
                     'diagnosis' => $c->diagnosis,
@@ -77,20 +81,23 @@ class CheckupsController extends Controller
         return $checkups;
     }
 
-    public function FindCheckupsByOwnerName(Request $request){
+    public function FindCheckupsByOwnerName(Request $request)
+    {
         $ownerName = $request->validate(['name' => 'string|required'])['name'];
-        $owners = Owner::where('firstName', 'like', '%'.$ownerName.'%')->orWhere('lastName', 'like', '%'.$ownerName.'%')->get();
+        $owners = Owner::where('firstName', 'like', '%' . $ownerName . '%')->orWhere('lastName', 'like', '%' . $ownerName . '%')->get();
         $checkups = [];
-        foreach($owners as $o){
-            foreach($o->patients as $p){
+        foreach ($owners as $o) {
+            foreach ($o->patients as $p) {
                 $t_c = $p->checkups()->orderBy('year', 'desc')->orderBy('month', 'desc')->orderBy('day', 'desc')->get();
-                foreach($t_c as $c){
+                foreach ($t_c as $c) {
                     $checkups[] = [
                         'id' => $c->id,
                         'name' => $p->name,
                         'owner' => $o->firstName . ' ' . $o->lastName,
                         'age' => $p->age,
                         'gender' => $p->gender == 0 ? 'نر' : 'ماده',
+                        'type' => $p->type()->get(['name'])[0]->name,
+                        'race' => $p->race()->get(['name'])[0]->name,
                         'date' => $c->year . '/' . $c->month . '/' . $c->day,
                         'sympthoms' => $c->sympthoms,
                         'diagnosis' => $c->diagnosis,
@@ -102,6 +109,7 @@ class CheckupsController extends Controller
         return $checkups;
     }
 
-    public function FindCheckupsByDate(Request $request){}
-
+    public function FindCheckupsByDate(Request $request)
+    {
+    }
 }
